7daf5492fb082b086f0acc2e8d3aea616da70f8b
Linda Claassen <claassenl@out.co.za>
2025-10-17 12:53:53 +0200

    Yml for Github Actions

--- main.py
+++ main.py
@@ -3,63 +3,46 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import date, datetime
 import logging
-import azure.functions as func
 
-# def main(mytimer: func.TimerRequest) -> None:
-#     logging.info("Azure Function triggered")
-#     try:
-#         my_app()
-#         logging.info("App ran successfully")
-#     except Exception as e:
-#         logging.error(f"Error running app: {e}")
+START_DATE = datetime.strptime("2025-10-01", "%Y-%m-%d").date()
+TODAY = date.today()
 
-def my_app():
-    START_DATE = datetime.strptime("2025-10-01", "%Y-%m-%d").date()
-    TODAY = date.today()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger(__name__)
+if  1==0:
+    # ((TODAY - START_DATE).days % 14 != 0 
+    # or (TODAY - START_DATE).days % (3*14) == 0
+    # ): # every 2nd week except every 6th week
+    logger.info(f"\n\nCriteria not met: {datetime.now()}\n\n")
 
-    with open("date_log.txt", "r") as file:
-        dates = file.readlines()
-        dates = [line.strip() for line in dates]
+else:
+    logger.info(f"\n\nCriteria met: {datetime.now()}\n\n")
 
-    if (TODAY.strftime("%Y-%m-%d") in dates # already run today
-        or (TODAY - START_DATE).days % 14 != 0 
-        or (TODAY - START_DATE).days % (3*14) == 0): # every 2nd week except every 6th week
-        logger.info(f"\n\nCriteria not met: {datetime.now()}\n\n")
+    # Email configuration
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    sender_email = "linda.claassen.89@gmail.com"
+    receiver_email = "linda.claassen.89@gmail.com " #brooklyn@tshwane.gov.za"
+    password = "isap rioe okdl xzwa"
 
-    else:
-        logger.info(f"\n\nCriteria met: {datetime.now()}\n\n")
+    # Create the email
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = "Please renew"
 
-        # Email configuration
-        smtp_server = "smtp.gmail.com"
-        smtp_port = 587
-        sender_email = "linda.claassen.89@gmail.com"
-        receiver_email = "brooklyn@tshwane.gov.za"
-        password = "isap rioe okdl xzwa"
+    # Email body
+    body = "Good morning<br><br>Can you please renew the books on these two cards for two more weeks?<br><br>400591012<br>400591014<br><br>Thanks, <br>Linda"
+    message.attach(MIMEText(body, "html"))
 
-        # Create the email
-        message = MIMEMultipart()
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Subject"] = "Please renew"
+    # Send the email
+    try:
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()  # Secure the connection
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+    except Exception as e:
+        with open("date_log.txt", "a") as file:
+            logger.error(f"{TODAY} failed: {e}\n")
 
-        # Email body
-        body = "Good morning<br><br>Can you please renew the books on these two cards for two more weeks?<br><br>400591012<br>400591014<br><br>Thanks, <br>Linda"
-        message.attach(MIMEText(body, "html"))
-
-        # Send the email
-        try:
-            with smtplib.SMTP(smtp_server, smtp_port) as server:
-                server.starttls()  # Secure the connection
-                server.login(sender_email, password)
-                server.sendmail(sender_email, receiver_email, message.as_string())
-            with open("date_log.txt", "a") as file:
-                file.write(f"{TODAY}\n")
-        except Exception as e:
-            with open("date_log.txt", "a") as file:
-                file.write(f"{TODAY} failed: {e}\n")
-
-
-# my_app()
